6b95d1a92726f4e4c4f8f8b5476d81b4ac4b9d4c
Avraham Micael <avrahammicael@protonmail.com>
2022-12-17 17:08:10 -0300

    image compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vsharp from "vite-plugin-vsharp"
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vsharp({
+        '.jpg': {
+            quality: 40,
+        },
+    })
+  ],
   build: {
     target: 'es2015',
   },
