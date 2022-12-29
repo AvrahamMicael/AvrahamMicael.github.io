e739a329e885270b393042bd6441aa767a81f3f8
Avraham Micael <avrahammicael@protonmail.com>
2022-12-29 18:42:22 -0300

    a2bc299

---
+++ assets/copyToClipboard.df77b233.js
@@ -0,0 +1,1 @@
+import{i as a}from"./index.f41a5340.js";const{t:i}=a.global,n=o=>{const t=o.target;navigator.clipboard.writeText(t.innerText),alert(i("functions.copy-to-clipboard.copied"))};export{n as default};
